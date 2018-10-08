34ab28af8455ca816fb3adcd112defd305309f36
Marc-Eric Pelletier <medaragon@hotmail.com>
2018-10-08 00:45:36 -0400

    Added a dropdown for the partitioner for the group types.

--- src/components/Partitioner.vue
+++ src/components/Partitioner.vue
@@ -1,13 +1,35 @@
 <template>
-  <h3>Partitioner</h3>
+  <div>
+    <h3>Partitioner</h3>
+    <v-select class="select" v-model="selectedGroupType" label="name" :options="groupTypes" placeholder="Type de groupe">
+
+    </v-select>
+    <button @click="createGroups">Create groups</button>
+  </div>
 </template>
 
 <script>
+import vSelect from 'vue-select'
 export default {
-  name: 'HelloWorld',
+  name: 'Partitioner',
+  components: {
+    vSelect
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      selectedGroupType: null,
+      groupTypes: [
+        { id: 2, name: 'Tutorat' },
+        { id: 3, name: 'Procédural' },
+        { id: 4, name: 'Projet' }
+      ]
+    }
+  },
+  methods: {
+    createGroups() {
+      if (this.selectedGroupType != null) {
+        alert(`Groupes de ${this.selectedGroupType.name} créés.`)
+      }
     }
   }
 }
