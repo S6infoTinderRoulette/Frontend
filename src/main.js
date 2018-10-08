863afc2ee99713579fc11a9025b943bb407b748a
Marc-Eric Pelletier <medaragon@hotmail.com>
2018-10-08 01:41:31 -0400

    Added a language manager.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import i18n from './setup/i18n-setup'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: 'app',
+  i18n,
   router,
   store,
   render: h => h(App)
