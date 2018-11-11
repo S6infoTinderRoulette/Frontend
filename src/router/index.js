15a118e9b247a0711a44a1fdba11409054fad9a9
Unknown <johany.camirand@usherbrooke.ca>
2018-11-10 20:51:48 -0500

    Add new GroupManager componant

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Partitioner from '@/components/Partitioner'
 import Matchmaking from '@/components/Matchmaking'
+import GroupManager from '@/components/GroupManager'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/matchmaking',
       name: 'Matchmaking',
       component: Matchmaking
+    },
+    {
+      path: '/GroupManager',
+      name: 'GroupManager',
+      component: GroupManager
     }
   ]
 })
