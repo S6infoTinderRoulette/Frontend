25dbfe2f5d72687fc64fd9b100f6c3fececebe54
Marc-Eric Pelletier <medaragon@hotmail.com>
2018-11-21 20:43:21 -0500

    Fix

--- src/main.js
+++ src/main.js
@@ -3,12 +3,6 @@ import App from './App'
 import router from './router'
 import store from './store'
 import i18n from './setup/i18n-setup'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-
-Vue.use(BootstrapVue);
 
 import VuejsDialog from "vuejs-dialog"
 import VuejsDialogMixin from "vuejs-dialog/dist/vuejs-dialog-mixin.min.js" // only needed in custom components
